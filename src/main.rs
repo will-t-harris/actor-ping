f32fc10a31a2ee9c1c2f60ebb55d054ba37215dc
Will Harris <will.t.harris@protonmail.com>
2020-08-23 11:12:35 -0700

    add actor example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use actix::prelude::*;
+
+#[derive(Message)]
+#[rtype(result = "usize")]
+struct Ping(usize);
+
+struct MyActor {
+    count: usize,
+}
+
+impl Actor for MyActor {
+    type Context = Context<Self>;
+}
+
+impl Handler<Ping> for MyActor {
+    type Result = usize;
+
+    fn handle(&mut self, msg: Ping, _ctx: &mut Context<Self>) -> Self::Result {
+        self.count += msg.0;
+
+        self.count
+    }
+}
+
+#[actix_rt::main]
+async fn main() {
+    // start actor
+    let addr = MyActor { count: 10 }.start();
+
+    // send message and get future for result
+    let res = addr.send(Ping(10)).await;
+
+    // handle() returns tokio handle
+    println!("RESULT: {}", res.unwrap() == 20);
+
+    // stop and exit
+    System::current().stop();
 }
